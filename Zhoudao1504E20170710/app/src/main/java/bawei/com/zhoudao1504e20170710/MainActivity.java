dce8ebc0f625c886db4eb4dbbcc6d493b04e2c27
zhoudaodao <875205514@qq.com>
2017-07-10 09:25:55 +0800

    zhoukao

---
+++ Zhoudao1504E20170710/app/src/main/java/bawei/com/zhoudao1504e20170710/MainActivity.java
@@ -0,0 +1,13 @@
+package bawei.com.zhoudao1504e20170710;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
